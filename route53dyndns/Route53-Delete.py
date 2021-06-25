6da43c366113ce68b1e0ce498ade2182ca9e99b6
dgmcswain <dgmcswain@gmail.com>
2021-06-24 23:34:11 -0400

    Update Route53-Delete.py

--- route53dyndns/Route53-Delete.py
+++ route53dyndns/Route53-Delete.py
@@ -8,7 +8,7 @@ from botocore.exceptions import ClientError
 def lambda_handler(event, context):
     route53 = boto3.client('route53')
     sqs = boto3.client('sqs')
-    queue_url = 'https://sqs.us-east-1.amazonaws.com/123456789012/Route53-Delete-HIDS-SQS.fifo'
+    queue_url = 'https://sqs.us-east-1.amazonaws.com/123456789012/Route53-Delete-SQS.fifo'
 
     # Define Check For Existing DNS Record
     def check_for_existing_dns(zone_id,record_name,rectype):
@@ -50,30 +50,30 @@ def lambda_handler(event, context):
             msg_body = get_msg['Messages'][0]['Body']
             msg_body = msg_body.replace("\'","\"")
             msg_body = json.loads(msg_body)
-            HCQISName = msg_body['HCQISName']
-            HCQISNameMgt = msg_body['HCQISNameMgt']
+            DNSName = msg_body['DNSName']
+            DNSNameMgt = msg_body['DNSNameMgt']
             action = msg_body['action']
             func_ip = msg_body['func_ip']
             mgmt_ip = msg_body['mgmt_ip']
             OpSys = msg_body['OpSys']
-            hosted_zone_name = "ado11.r53test.org."
-            hosted_zone_id = "Z0362588342LPQBCO20TC"
-            print(HCQISName,action,func_ip,mgmt_ip)
+            hosted_zone_name = "r53test.org."
+            hosted_zone_id = "Z123456789ABCDEFGHIJKL"
+            print(DNSName,action,func_ip,mgmt_ip)
             if OpSys == "CentOS" or OpSys == "RHEL":
                 # Build DNS Record Sets
                 if func_ip == mgmt_ip:
                     rectype = ['A','CNAME']
                     zone_id = hosted_zone_id
-                    value = [func_ip,HCQISName + '.' + hosted_zone_name]
-                    record_name = [HCQISName + '.' + hosted_zone_name, HCQISNameMgt + '.' + hosted_zone_name]
+                    value = [func_ip,DNSName + '.' + hosted_zone_name]
+                    record_name = [DNSName + '.' + hosted_zone_name, DNSNameMgt + '.' + hosted_zone_name]
                 else:
                     rectype = ['A','A']
                     zone_id = hosted_zone_id
                     value = [func_ip,mgmt_ip]
-                    record_name = [HCQISName + '.' + hosted_zone_name, HCQISNameMgt + '.'+ hosted_zone_name]
+                    record_name = [DNSName + '.' + hosted_zone_name, DNSNameMgt + '.'+ hosted_zone_name]
                 for i in [0,1]:
                     if check_for_existing_dns(zone_id,record_name[i],rectype[i]) == 'NOERROR':
-                        print("Deleting DNS records for " + HCQISName + ", " + HCQISNameMgt )
+                        print("Deleting DNS records for " + DNSName + ", " + DNSNameMgt )
                         change_resource_record(zone_id,action,record_name[i],rectype[i],value[i])
                         time.sleep(0.3)
                         recset = [zone_id,action,record_name[i],rectype[i],value[i]]
@@ -86,17 +86,17 @@ def lambda_handler(event, context):
                         else:
                             print(record_name[i] + " " + "'" + rectype[i] + "'" + " record deleted.")
             # Delete PTR Records
-            rev_lookup_zone_id = 'Z03540141GKWIKLPTXOMS'
+            rev_lookup_zone_id = 'Z123456789ABCDEFGHIJKL'
             rev_lookup_zone_name = '10.in-addr.arpa'
             rev_func_ip = msg_body['rev_func_ip']
             rev_mgmt_ip = msg_body['rev_mgmt_ip']
             rectype = 'PTR'
             zone_id = rev_lookup_zone_id
-            value = [HCQISName + '.' + hosted_zone_name, HCQISNameMgt + '.'+ hosted_zone_name]
+            value = [DNSName + '.' + hosted_zone_name, DNSNameMgt + '.'+ hosted_zone_name]
             record_name = [rev_func_ip + '.' + rev_lookup_zone_name, rev_mgmt_ip + '.' + rev_lookup_zone_name]
             for i in [0,1]:
                 if check_for_existing_dns(zone_id,record_name[i],rectype) == 'NOERROR':
-                    print("Deleting DNS records for " + HCQISName + ", " + HCQISNameMgt )
+                    print("Deleting DNS records for " + DNSName + ", " + DNSNameMgt )
                     change_resource_record(zone_id,action,record_name[i],rectype,value[i])
                     recset = [zone_id,action,record_name[i],rectype,value[i]]
                     print(recset)
